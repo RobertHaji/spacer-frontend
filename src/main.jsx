0d54274ae717b1499d4fd53a305df13751fc59b9
Bildad Ereggae <bildadereggae@gmail.com>
2025-07-20 22:53:25 +0300

    added sign up form

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,20 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 import LoginPage from "./pages/login";
 import App from "./App";
-
+import SignUpPage from "./pages/signup";
 
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: <App />,
   },
   {
     path: "/login",
-    element: <LoginPage/>
+    element: <LoginPage />,
+  },
+  {
+    path: "signup",
+    element: <SignUpPage />,
   },
 ]);
 
---
+++ src/pages/signup.jsx
@@ -0,0 +1,11 @@
+import { SignUpForm } from "@/components/signup-form";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex w-full h-full justify-center items-center">
+      <div className="max-w-sm">
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}
