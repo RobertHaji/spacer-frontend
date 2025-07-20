e538c3de23b3b3e91be5ee5e161ec1e1917fa8b8
Bildad Ereggae <bildadereggae@gmail.com>
2025-07-20 22:41:51 +0300

    created routes and added log in in form

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import LoginPage from "./pages/login";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={routes} />
+  </StrictMode>
+);
