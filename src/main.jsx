10dc4fb6b68a214f0fc2b45afa88ea51d33566fc
Tabitha Wangechi <twangechi742@gmail.com>
2025-07-21 09:39:37 +0300

    created categories-form.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import LoginPage from "./pages/login";
 import App from "./App";
 import SignUpPage from "./pages/signup";
+import 
 
 const routes = createBrowserRouter([
   {
