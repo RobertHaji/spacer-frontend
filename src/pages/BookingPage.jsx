f3d09442a4604822bf5c0f9c327b65dcbd50421e
arifrahim <arifrahimjamal@gmail.com>
2025-07-22 14:31:24 +0300

    created a booking page

---
+++ src/pages/BookingPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "@/components/ui/Header";
+import Footer from "@/components/ui/Footer";
+import { BookingsPage } from "@/components/Bookingshistory";
+<>
+<Header />
+    <BookingsPage/>
+<Footer />
+</>
