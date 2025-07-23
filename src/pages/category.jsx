0a78a7bda8450476e1b513badc3c9eb73abb0f5b
Robert Haji <roberthadji66@gmail.com>
2025-07-23 03:10:14 +0300

    adds a single category in database using postman for testing SpaceForm

--- src/pages/category.jsx
+++ src/pages/category.jsx
@@ -20,31 +20,31 @@ const categories = [
     name: "Meetings",
     image:
       "https://www.cvent.com/venues/_next/image?url=https%3A%2F%2Fimages.cvent.com%2Fcsn%2Fbe3326cb-8d44-4140-8c3b-9e8327cd7282%2Fimages%2Fb280d5c66652417b9790284393edd581_large!_!fad642b3cabe2148c1be1d55f8ece857.jpg&w=3840&q=30",
-    //slug: "meetings",
+    slug: "meetings",
   },
   {
     name: "Events",
     image:
       "https://www.oyorooms.com/blog/wp-content/uploads/2018/02/type-of-event.jpg",
-    //slug: "events",
+    slug: "events",
   },
   {
     name: "Baby shower",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4GGzUbOksPoT65XkMEGrCw3_FPy0uF2_YwbzrEvZxWG_GoqKzhe_BVaZ_ZLSwyt5fPkE&usqp=CAU",
-    // slug: "baby-shower",
+    slug: "baby-shower",
   },
   {
     name: "Graduation party",
     image:
       "https://t3.ftcdn.net/jpg/11/38/78/46/360_F_1138784636_PAJFSpLX3DFoB7kT9hnmfmmhYuTSUPED.jpg",
-    // slug: "graduation-party",
+    slug: "graduation-party",
   },
   {
     name: "Birthday",
     image:
       "https://elegantlivingeveryday.com/wp-content/uploads/2023/10/How-to-Plan-an-Adult-Birthday-Party-Featured-Image.jpg",
-    // slug: "birthday",
+    slug: "birthday",
   },
 ];
 
